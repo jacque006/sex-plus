f157bb29c8650949689fc580cfe10586ff5c814c
Chris Hague <chris@t510debian>
2018-02-18 01:10:00 -0700

    added std and birthctr rates to medical contract

--- src/contracts/medical_record.sol
+++ src/contracts/medical_record.sol
@@ -64,4 +64,52 @@ contract MedicalRecord {
             data.submitDate = submitDate;
             data.expDate = expDate;
         }
+
+    // chance of user having std
+    function stdChance()
+        public
+        userOrProvider
+        returns(int)
+        {
+            // 0% chance at expireHour
+            int expireHour = 48;
+
+            int std = 0;
+            if (data.std) {
+                return 99;
+            } else {
+                uint diff = now - data.submitDate;
+                uint sec = diff/1000;
+                uint min = sec/60;
+                uint hr = min/60;
+
+                std = (int(hr) * 99) / expireHour;
+            }
+            return std;
+        }
+
+    // chance of user using birthcontrol
+    function birthControlChance()
+        public
+        userOrProvider
+        returns(int)
+        {
+            // 0% chance at expireHour
+            int expireHour = 48;
+
+            int birthCtr = 0;
+            if (now < data.expDate && data.birthCtr) {
+                return 99;
+            } else if (!data.birthCtr) {
+                return 0;
+            } else {
+                uint diff = now - data.expDate;
+                uint sec = diff / 1000;
+                uint min = sec / 60;
+                uint hr = min / 60;
+
+                birthCtr = (int(hr) * 99 / expireHour);
+            }
+            return birthCtr;
+        }
 }
